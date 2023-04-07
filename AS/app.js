910e292a56d8dc039dbc0d6bbd830d15de0f37c4
khangndph20612 <khangndph20612@fpt.edu.vn>
2023-04-08 02:59:23 +0700

    1

--- AS/app.js
+++ AS/app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose')
+const session = require('express-session');
+const passport = require('passport');
 const url = 'mongodb+srv://khangndph:2992003@atlascluster.duylrou.mongodb.net/asm?retryWrites=true&w=majority'
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,19 +14,15 @@ const helmet = require('helmet');
 
 const methodOverride = require('method-override');
 const app = express();
-
+app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(methodOverride('_method'));
-app.use((req, res, next) => {
-    if (req.body && req.body._method) {
-        req.method = req.body._method;
-        delete req.body._method;
-    }
-    next();
-});
+
+
+
 app.use(express.json());
 mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log('Connected to MongoDB'))
--- AS/routes/users.js
+++ AS/routes/users.js
@@ -4,13 +4,38 @@ const User = require('../models/user');
 var multer = require('multer');
 const Jimp = require('jimp');
 const url = require('url');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const jwt = require('jsonwebtoken');
 
 /* GET users listing. */
-router.get('/list', async (req, res, next) => {
+function isAuthenticated() {
+    return function (req, res, next) {
+        if (req.isAuthenticated()) {
+            return next();
+        }
+        res.redirect('http://localhost:3000/');
+    }
+}
+
+
+// Đăng nhập thành công, lưu thông tin vào session
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+// Xác thực người dùng từ session
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+router.get('/list',authenticateToken, async (req, res, next) => {
     const users = await User.find({})
 
     try {
-        res.render('users', {users: users });
+        res.render('users', {users: users});
     } catch (err) {
         res.status(500).send(err)
     }
@@ -19,12 +44,12 @@ router.get('/:id', async (req, res) => {
     try {
         const user = await User.findById(req.params.id);
         if (!user) {
-            return res.status(404).send({ error: 'User not found' });
+            return res.status(404).send({error: 'User not found'});
         }
-        res.status(200).send({ user });
+        res .send({user});
     } catch (error) {
         console.error(error);
-        res.status(500).send({ error: 'Server error' });
+        res.status(500).send({error: 'Server error'});
     }
 });
 const upload = multer({dest: 'uploads/'}); // định nghĩa thư mục tạm để lưu file
@@ -36,17 +61,16 @@ router.post('/update/:id', upload.single('image'), async (req, res) => {
         user.name = name;
         user.password = password;
         user.email = email;
-       if (req.file !== null){
-           const imagePath = req.file.path; // lấy đường dẫn tới file từ đối tượng req.file
-           const image = await Jimp.read(imagePath);
-           // resize ảnh về kích thước 100x100px
-           const base64Image = await image.getBase64Async(Jimp.MIME_PNG);
-           user.image = base64Image;
-       }
+        if (req.file !== null) {
+            const imagePath = req.file.path; // lấy đường dẫn tới file từ đối tượng req.file
+            const image = await Jimp.read(imagePath);
+            // resize ảnh về kích thước 100x100px
+            const base64Image = await image.getBase64Async(Jimp.MIME_PNG);
+            user.image = base64Image;
+        }
 // Lưu lại dữ liệu đã update
         await user.save();
         res.redirect('http://localhost:3000/users/list')
-
 
 
     } catch (error) {
@@ -67,72 +91,91 @@ router.delete('/delete/:id', async (req, res) => {
     }
 });
 router.post('/login', async (req, res) => {
-    const { email, password } = req.body;
+    const {email, password} = req.body;
 
     try {
         // Tìm kiếm tài khoản người dùng với email tương ứng
-        const user = await User.findOne({ email });
+        const user = await User.findOne({email});
 
         if (!user) {
             // Nếu không tìm thấy tài khoản, trả về thông báo lỗi
-            return res.status(400).json({ message: 'Invalid email or password' });
+            return res.status(400).json({message: 'Invalid email or password'});
         }
 
         // Kiểm tra mật khẩu
 
 
-        if (password!==user.password) {
+        if (password !== user.password) {
             // Nếu mật khẩu không khớp, trả về thông báo lỗi
-            return res.status(400).json({ message: 'Invalid email or password' });
+            return res.status(400).json({message: 'Invalid email or password'});
         }
+        // Tạo token
+        const token = jwt.sign({_id: user._id}, "mk");
 
-        // Nếu thông tin đăng nhập chính xác, tạo session và chuyển hướng đến trang chủ
-         res.redirect('http://localhost:3000/users/list')
+        // Thiết lập cookie trên header
+        res.setHeader('Set-Cookie', `token=${token}; HttpOnly`);
+
+        // Trả về kết quả
+       
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: 'Server error' });
+        res.status(500).json({message: 'Server error'});
     }
 });
+
 router.post('/', upload.single('image'), async (req, res, next) => {
     const imagePath = req.file.path; // lấy đường dẫn tới file từ đối tượng req.file
     const image = await Jimp.read(imagePath);
     // resize ảnh về kích thước 100x100px
-    const base64Image = await image.getBase64Async(Jimp.MIME_PNG);
+    const base64Image = await image.getBase64Async(Jimp.AUTO);
     // xử lý file ở đây
 
-    const {text}=req.body
+    const {text} = req.body
 
     const newUser = new User(req.body);
     newUser.image = base64Image;
     try {
-        await newUser.save()
-        if(text==='s'){
+        const x = await newUser.save();
+
+        if (text === 's') {
             res.redirect('http://localhost:3000/users/list')
-        }else{
+        } else {
             res.redirect('/')
         }
-     } catch (err) {
+    } catch (err) {
         res.status(500).send(err)
     }
 });
 router.post('/search', async (req, res) => {
-    const { search } = req.body;
+    const {search} = req.body;
 
     try {
         // Tìm kiếm trong cơ sở dữ liệu
         const users = await User.find({
             $or: [
-                { firstName: { $regex: search, $options: 'i' } },
-                { lastName: { $regex: search, $options: 'i' } },
-                { email: { $regex: search, $options: 'i' } },
+                {firstName: {$regex: search, $options: 'i'}},
+                {lastName: {$regex: search, $options: 'i'}},
+                {email: {$regex: search, $options: 'i'}},
             ]
         });
 
-         res.render('users', { users:users });
+        res.render('users', {users: users});
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: 'Server error' });
+        res.status(500).json({message: 'Server error'});
     }
 });
+function authenticateToken(req, res, next) {
+    const token = req.cookies.token;
 
+    if (token == null) return res.redirect('http://localhost:3000/');
+
+    jwt.verify(token, 'mk', (err, user) => {
+        if (err) return res.sendStatus(403);
+
+        req.user = user;
+
+        next();
+    });
+}
 module.exports = router;
